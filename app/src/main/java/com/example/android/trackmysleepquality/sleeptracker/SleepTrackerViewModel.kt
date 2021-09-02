10e2743cfa660aefa88b39bfdab56a3ac5615964
Satrio Wisnu Wardono <satrio.wisnu21@gmail.com>
2021-09-02 16:27:59 +0700

    coroutines

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -17,9 +17,16 @@
 package com.example.android.trackmysleepquality.sleeptracker
 
 import android.app.Application
+import android.provider.SyncStateContract.Helpers.insert
+import android.provider.SyncStateContract.Helpers.update
 import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import com.example.android.trackmysleepquality.database.SleepDatabaseDao
+import com.example.android.trackmysleepquality.database.SleepNight
+import com.example.android.trackmysleepquality.formatNights
+import kotlinx.coroutines.*
 
 /**
  * ViewModel for SleepTrackerFragment.
@@ -27,5 +34,95 @@ import com.example.android.trackmysleepquality.database.SleepDatabaseDao
 class SleepTrackerViewModel(
         val database: SleepDatabaseDao,
         application: Application) : AndroidViewModel(application) {
+
+//      TODO (01) Create a viewModelJob and override onCleared() for canceling coroutine.
+        private var viewModelJob = Job()
+
+        override fun onCleared() {
+                super.onCleared()
+                viewModelJob.cancel()
+        }
+
+//      TODO (02) Define a scope for the coroutine to run in.
+        private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+
+//      TODO (03) Create tonight Live data var and use a coroutine to initialize it from the database.
+        private val tonight = MutableLiveData<SleepNight?>()
+
+//      TODO (04) Get all nights from the database.
+        private val nights = database.getAllNights()
+
+        val nightsString = Transformations.map(nights) { nights ->
+                formatNights(nights, application.resources)
+        }
+
+        init {
+            initializeTonight()
+        }
+
+        private fun initializeTonight() {
+                uiScope.launch {
+                        tonight.value = getTonightFromDatabase()
+                }
+        }
+
+        private suspend fun getTonightFromDatabase(): SleepNight? {
+                return withContext(Dispatchers.IO) {
+                        var night = database.getTonight()
+                        if (night?.endTimeMilli != night?.startTimeMilli){
+                                night = null
+                        }
+                        night
+                }
+        }
+
+//      TODO (05) Add local functions for insert(), update(), and clear().
+//      TODO (06) Implement handlers for Start, Stop,and Clear button using coroutines to do the database work.
+        fun onStartTracking(){
+                uiScope.launch {
+
+                        val newNight = SleepNight()
+
+                        insert(newNight)
+
+                        tonight.value = getTonightFromDatabase()
+                }
+        }
+
+        private suspend fun insert(night: SleepNight) {
+                withContext(Dispatchers.IO) {
+                        database.insert(night)
+                }
+        }
+
+        fun onStopTracking() {
+                uiScope.launch {
+                        val oldNight = tonight.value ?: return@launch
+
+                        oldNight.endTimeMilli = System.currentTimeMillis()
+
+                        update(oldNight)
+                }
+        }
+
+        private suspend fun update(night: SleepNight) {
+                withContext(Dispatchers.IO) {
+                        database.update(night)
+                }
+        }
+
+        fun onClear() {
+                uiScope.launch {
+                        clear()
+                        tonight.value = null
+                }
+        }
+
+        suspend fun clear() {
+                withContext(Dispatchers.IO) {
+                        database.clear()
+                }
+        }
+//      TODO (09) Transfer nights into a nightString using formatNights().
 }
 
